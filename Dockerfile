410b7c40eb07b0177b07aa3544c4255ae774b7b5
ryan.steed.usa <ryan.steed.usa@pm.me>
2026-04-04 12:28:13 -0600

    chore: update to 6.3

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 
 FROM eclipse-temurin:21-jdk
 
-LABEL version="6.2"
+LABEL version="6.3"
 
 
 RUN apt-get update && apt-get install -y curl unzip jq && \
--- launch.sh
+++ launch.sh
@@ -2,8 +2,8 @@
 set -x
 
 
-NEOFORGE_VERSION=21.1.221
-SERVER_VERSION=6.2
+NEOFORGE_VERSION=21.1.222
+SERVER_VERSION=6.3
 
 cd /data
 
@@ -17,7 +17,7 @@ fi
 if ! [[ -f "Server-Files-$SERVER_VERSION.zip" ]]; then
     rm -fr config defaultconfigs kubejs mods packmenu Server-Files-* neoforge*
 
-    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7807/57/ServerFiles-$SERVER_VERSION.zip" || exit 9
+    curl -Lo "Server-Files-$SERVER_VERSION.zip" "https://mediafilez.forgecdn.net/files/7865/483/ServerFiles-$SERVER_VERSION.zip" || exit 9
   
     unzip -u -o "Server-Files-$SERVER_VERSION.zip" -d /data
     DIR_TEST="ServerFiles-$SERVER_VERSION"
